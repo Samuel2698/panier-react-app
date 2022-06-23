6ed08cdf728e843c54bd75c851569741e11ce306
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-23 11:02:41 +0200

    Update App.styles.ts

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -8,7 +8,8 @@ export const Wrapper = styled.div`
 export const StyledButton = styled(IconButton)`
     position: fixed;
     z-index: 100;
-    right: 20px;
-    top: 20px;
+    right: 10px;
+    top: 10px;
+    padding: 10px;
 `
 
